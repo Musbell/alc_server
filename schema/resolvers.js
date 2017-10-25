44a79788776222c5318435fef4ef2a24504a1b59
Bello Musa <musbell007@gmail.com>
2017-10-25 23:03:13 +0100

    create server

---
+++ schema/resolvers.js
@@ -0,0 +1,71 @@
+const pubsub = require('../pubsub');
+
+
+function buildFilters({ OR = [], firstNameContains, lastNameContains }) {
+  const filter = (firstNameContains || lastNameContains) ? {} : null;
+  if (firstNameContains) {
+    filter.firstName = { $regex: `.*${firstNameContains}.*` };
+  }
+  if (lastNameContains) {
+    filter.lastName = { $regex: `.*${lastNameContains}.*` };
+  }
+
+  let filters = filter ? [filter] : [];
+  for (let i = 0; i < OR.length; i++) {
+    filters = filters.concat(buildFilters(OR[i]));
+  }
+  return filters;
+}
+
+
+const resolvers = {
+  Query: {
+    allStudents: async (root, { filter, first, skip }, { models }) => { // 1
+      const query = filter ? { $or: buildFilters(filter) } : {};
+      const cursor = models.Student.findAll(query);
+      if (first) {
+        cursor.limit(first);
+      }
+      if (skip) {
+        cursor.skip(skip);
+      }
+      return cursor;
+    },
+    Student: async (root, { id }, { models }) => await models.Student.findOne({
+      where: id,
+    }),
+  },
+  Mutation: {
+    createStudent: async (root, args, { models }) => {
+      const newStudent = await models.Student.create(args); // 3
+
+      pubsub.publish('Student', { student: { mutation: 'CREATED', node: newStudent } });
+
+      return newStudent;
+    },
+    updateStudent: async (root, args, { models }) => {
+      const updatedStudent = await models.Student.update(args); // 3
+
+      pubsub.publish('Student', { student: { mutation: 'UPDATED', node: updatedStudent } });
+
+      return updatedStudent;
+    },
+    deleteStudent: async (root, args, { models }) => {
+      const deletedStudent = await models.Student.destroy({
+        where: args,
+      });
+
+      pubsub.publish('Student', { student: { mutation: 'DELETED', node: deletedStudent } });
+
+      return deletedStudent;
+    },
+  },
+  Subscription: {
+    Student: {
+      subscribe: () => pubsub.asyncIterator('Student'),
+    },
+  },
+};
+
+
+module.exports = resolvers;
---
+++ schema/typeDefs.js
@@ -0,0 +1,107 @@
+// define types
+const typeDefs = `
+type Student {
+  id: ID!
+  lastName: String!
+  firstName: String!
+  otherName: String!
+  gender: String!
+  dateOfBirth: String!
+  nationality: String!
+  sessionOfAdmission: String!
+  dateOfAdmission: String!
+  stateOfOrigin: String!
+  religion: String!
+  address: String!
+  city: String!
+  state: String!
+  email: String!
+  phone: String!
+  createdAt: String!
+  updatedAt: String!
+}
+type Query {
+  allStudents(filter: StudentFilter, skip: Int, first: Int): [Student!]!
+  Student(id: ID!): Student
+}
+input StudentFilter {
+  OR: [StudentFilter!]
+  lastName_contains: String
+    firstName_contains: String
+}
+type Mutation {
+  createStudent(
+    lastName: String!
+    firstName: String!
+    otherName: String!
+    gender: String!
+    dateOfBirth: String!
+    nationality: String!
+    sessionOfAdmission: String!
+    dateOfAdmission: String!
+    stateOfOrigin: String!
+    religion: String!
+    address: String!
+    city: String!
+    state: String!
+    email: String!
+    phone: String!
+  ): Student
+  
+   updateStudent(
+    lastName: String
+    firstName: String
+    otherName: String
+    gender: String
+    dateOfBirth: String
+    nationality: String
+    sessionOfAdmission: String
+    dateOfAdmission: String
+    stateOfOrigin: String
+    religion: String
+    address: String
+    city: String
+    state: String
+    email: String
+    phone: String,
+    newLastName: String
+    newFirstName: String
+    newOtherNames: String
+    newGender: String
+    newDateOfBirth: String
+    newNationality: String
+    newSessionOfAdmission: String
+    newDateOfAdmission: String
+    newStateOfOrigin: String
+    newReligion: String
+    newAddress: String
+    newCity: String
+    newState: String
+    newEmail: String
+    newPhone: String
+  ): [Int!]!
+  
+  deleteStudent(id: ID!): Int!
+  }
+  
+
+type Subscription {
+   Student(filter: StudentSubscriptionFilter): StudentSubscriptionPayload
+}
+
+input StudentSubscriptionFilter {
+   mutation_in: [_ModelMutationType!]
+}
+
+type StudentSubscriptionPayload {
+   mutation: _ModelMutationType!
+   node: Student
+}
+
+enum _ModelMutationType {
+  CREATED
+  UPDATED
+  DELETED
+}
+`;
+module.exports = typeDefs;
